f14eaeaa01c2d69bfa5d7ea03902479e37a3143b
Lawmsangi <ddarkphoenixx22@gmail.com>
2024-01-29 23:51:11 +0530

    Update routes using routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
+import Categories from './components/Categories';
+import CategoryDetails from './components/CategoryDetails';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/categories/:title" element={<CategoryDetails />} />
+      </Routes>
+
     </div>
   );
 }
