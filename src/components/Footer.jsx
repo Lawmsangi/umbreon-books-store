630357efdee0b7c1fc165eebec77fdf6d7d1325e
Lawmsangi <ddarkphoenixx22@gmail.com>
2024-02-04 23:08:33 +0530

    Make the link work

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,22 +11,22 @@ function Footer() {
     <div className='footer'>
         <div className="footer-links">
             <div className="footer-icons">
-                <Link href="https://wa.me/6009766073" className='footer-link'>
+                <Link to="https://wa.me/6009766073" className='footer-link'>
                     <span>Contact via WhatsApp </span>
                     <FaWhatsapp className='footer-icon whatsapp-icon'/>
                 </Link>
-                <Link href="tel:+916009766073" className='footer-link'>
+                <Link to="tel:+916009766073" className='footer-link'>
                     <span>Call Us </span>
                     <TbPhoneCall className='footer-icon call-icon'/>
                 </Link>
             </div>
 
             <div className="footer-icons">
-                <Link href="mailto:lawmsangivarte22@gmail.com" className='footer-link'>
+                <Link to="mailto:lawmsangivarte22@gmail.com" className='footer-link'>
                     <span>Send Us an Email </span>
                     <CgMail className='footer-icon email-icon'/>
                 </Link>
-                <Link href="#" className='footer-link'>
+                <Link to="https://chat.whatsapp.com/I4zKTJVIXAq9AypaTxhjmk" className='footer-link'>
                     <span>Join Our Community </span>
                     <PiWechatLogoBold className='footer-icon whatsapp-icon'/>
                 </Link>
