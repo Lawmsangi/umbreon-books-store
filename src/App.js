c9dd28bcd538b9f2882bb300a94ff46699c4a74f
Lawmsangi <ddarkphoenixx22@gmail.com>
2024-02-04 17:22:38 +0530

    Add routes for bookDetails

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './components/Home';
 import Categories from './components/Categories';
 import CategoryDetails from './components/CategoryDetails';
+import BooksDetails from './components/BooksDetails';
 
 function App() {
   return (
@@ -10,9 +11,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/categories" element={<Categories />} />
-        <Route path="/categories/:title" element={<CategoryDetails />} />
+        <Route path="/categories/:category" element={<CategoryDetails />} />
+        <Route path="/categories/:category/:title" element={<BooksDetails/>} />
       </Routes>
-
     </div>
   );
 }
