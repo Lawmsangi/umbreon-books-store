0f33f809af772f29b7a24410962f035938855a3f
Lawmsangi <ddarkphoenixx22@gmail.com>
2024-02-04 17:23:19 +0530

    Change the params from title to category

--- src/components/CategoryDetails.jsx
+++ src/components/CategoryDetails.jsx
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import Books from './books.json';
 import '../styles/CategoryDetails.css';
 import ReactPaginate from 'react-paginate';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
-
 function CategoryDetails() {
-  const { title } = useParams();
+  const { category } = useParams();
+  console.log("Category:", category);
   const [currentPage, setCurrentPage] = useState(1);
   const booksPerPage = 10;
 
-  const filteredBooks = Books.books.filter(book => book.categories.includes(title));
+  const filteredBooks = Books.books.filter(book => book.categories.includes(category));
+  console.log(filteredBooks);
   // Calculate the index range of books to display based on the current page
   const startIndex = (currentPage -1) * booksPerPage;
   const endIndex = startIndex + booksPerPage;
@@ -29,15 +30,21 @@ function CategoryDetails() {
 
   return (
     <div className='categoryDetails'>
-      <h1>{title}({filteredBooks.length})</h1>
+      <h1>{category}({filteredBooks.length})</h1>
       <span>{`Page ${currentPage}`}</span>
       <div className='details-container'>
         {displayedBooks.map(book => (
-          <div className='details-content' key={book.title}>
-            <img src={book.img} alt={book.title} />
-            <h4>{book.title} by {book.author}</h4>
-            <p><span className='strike'>₹{book.mrp}</span><span className='our-price'> ₹{book.ourPrice}</span></p>
-          </div>
+          <Link to={`/categories/category/${book.title}`} key={book.title}>
+            <div className='details-content' key={book.title}>
+              <div className="details-img">
+                <img src={book.img} alt={book.title} />
+              </div>
+              <div className="details-name">
+                <h4>{book.title} by {book.author}</h4>
+                <p><span className='strike'>₹{book.mrp}</span><span className='our-price'> ₹{book.ourPrice}</span></p>
+              </div>
+            </div>
+          </Link>
         ))}
       </div>
         <ReactPaginate
