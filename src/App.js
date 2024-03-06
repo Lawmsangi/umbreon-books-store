46b2bb68cb1c691a18b76a3189a9c7b5ee59790b
Lawmsangi <ddarkphoenixx22@gmail.com>
2024-03-06 23:39:49 +0530

    Add checkout page

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import Home from './components/Home';
 import Categories from './components/Categories';
 import CategoryDetails from './components/CategoryDetails';
 import BooksDetails from './components/BooksDetails';
+import Checkout from './components/Checkout';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/categories" element={<Categories />} />
-        <Route path="/categories/:category" element={<CategoryDetails />} />
-        <Route path="/categories/:category/:title" element={<BooksDetails/>} />
+        <Route path="/" element={<><Navbar/><Home /><Footer/></>} />
+        <Route path="/categories" element={<><Navbar/><Categories /><Footer/></>} />
+        <Route path="/categories/:category" element={<><Navbar/><CategoryDetails /></>} />
+        <Route path="/categories/:category/:title" element={<><Navbar/><BooksDetails/></>} />
+        <Route path="/checkout" element={<><Navbar/><Checkout /></>} />
       </Routes>
     </div>
   );
