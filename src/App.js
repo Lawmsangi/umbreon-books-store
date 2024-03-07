639715ec168cb25fd8aaa4c8d15db4220db59218
Lawmsangi <ddarkphoenixx22@gmail.com>
2024-03-07 22:12:09 +0530

    Add payment component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import BooksDetails from './components/BooksDetails';
 import Checkout from './components/Checkout';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Payment from './components/Payment';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/categories/:category" element={<><Navbar/><CategoryDetails /></>} />
         <Route path="/categories/:category/:title" element={<><Navbar/><BooksDetails/></>} />
         <Route path="/checkout" element={<><Navbar/><Checkout /></>} />
+        <Route path="/payment" element={<><Payment /><Footer/></>} />
       </Routes>
     </div>
   );
