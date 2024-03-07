2ea80777f97a789f223d32ae13a24718ea9bda65
Lawmsangi <ddarkphoenixx22@gmail.com>
2024-03-07 22:13:08 +0530

    Add link on image

--- src/components/CheckoutProduct.jsx
+++ src/components/CheckoutProduct.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { removeFromCart } from '../redux/cartsSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import '../styles/CheckoutProduct.css'
-import QuantityDropdown from './QuantityDropdown';
+import { Link } from 'react-router-dom';
+// import QuantityDropdown from './QuantityDropdown';
 
-const CheckoutProduct = ({ item }) => {
+const CheckoutProduct = ({ item, index, title, image,mrp, price,author }) => {
   const dispatch = useDispatch()
   const { cart } = useSelector((store)=>store.cart)
 
@@ -13,16 +14,22 @@ const CheckoutProduct = ({ item }) => {
   };
   return (
     <div className="checkout-product">
-        <img className="checkout-product__image" src={item.image} alt={item.title} />
+      <Link to={`/categories/category/${title}`}>
+        <img className="checkout-product__image" src={image} alt={title} />
+      </Link>
         <div className="checkout-product__info">
-          <p className="checkout-product__title">{item.title}</p>
-          <p className="checkout-product__title">by {item.author}</p>
-          <p className="checkout-product__price">₹{item.price}</p>
-          <QuantityDropdown 
+          <p className="checkout-product__title">{title}</p>
+          <p className="checkout-product__title">by {author}</p>
+          <p><span className='strike'>₹{mrp}</span><span className='our-price'> ₹{price}</span></p>
+          {/* <QuantityDropdown 
            item={item}
-          />
-          <p className="checkout-product__price">₹</p>
-          <button onClick={handleRemoveFromCart}>Remove from Cart</button>
+          /> */}
+          <button 
+            className='remove-btn'
+            onClick={handleRemoveFromCart}
+          >
+            Remove from Cart
+          </button>
         </div>
     </div>
   );
