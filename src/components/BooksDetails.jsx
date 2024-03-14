842e2009098ddf6a6235ccbaad569df236658865
Lawmsangi <ddarkphoenixx22@gmail.com>
2024-03-14 11:51:16 +0530

    Add wishlist to bookdetails

--- src/components/BooksDetails.jsx
+++ src/components/BooksDetails.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Books from './books.json';
 import '../styles/BooksDetails.css';
 import Rating from './Rating';
-import { useDispatch } from 'react-redux';
-import { addToCart } from '../redux/cartsSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart, addToWishlist } from '../redux/cartsSlice';
+import { FaHeart } from 'react-icons/fa';
 
 function BookDetails() {
   const { title } = useParams();
   const dispatch = useDispatch();
+  const { wishlist } = useSelector((store)=>store.cart) 
+  const [alertBooks, setAlertBooks] = useState([]); 
 
   const filteredBooks = Books.books.filter(book => book.title.includes(title));
 
@@ -16,12 +19,43 @@ function BookDetails() {
     return <div>No books found for the title: {title}</div>;
   }
 
+  const handleAddToWishlist = (book) => {
+    dispatch(addToWishlist(book));
+    const isBookInWishlist = wishlist.some(item => item.title === book.title);
+    if (!isBookInWishlist) {
+        setAlertBooks(prevAlertBooks => [...prevAlertBooks, { book, type: 'added' }]);
+    } else {
+        setAlertBooks(prevAlertBooks => [...prevAlertBooks, { book, type: 'alreadyInWishlist' }]);
+    }
+     setTimeout(() => {
+        setAlertBooks(prevAlertBooks => prevAlertBooks.filter(item => item.book !== book));
+    }, 3000);
+  };
+
   return (
     <div>
       {filteredBooks.map(book => (
         <div key={book.title} className='books-details'>
           <div className='books-image'>
             <img src={book.img} alt={book.title} />
+            <div className="details-wishlist-container">
+              <button
+                className={`details-wishlist-icon ${wishlist.some(item => item.title === book.title) ? 'in-wishlist' : ''}`}
+                onClick={() => handleAddToWishlist(book)}
+              >
+                <FaHeart />
+              </button>
+            </div>
+            {alertBooks.some(item => item.book === book && item.type === 'added') && (
+              <div className="wishlist-alert" >
+                {`Added "${book.title}" to wishlist!`}
+              </div>
+            )}
+            {alertBooks.some(item => item.book === book && item.type === 'alreadyInWishlist') && (
+              <div className="wishlist-alert" >
+                {`"${book.title}" is already in the wishlist!`}
+              </div>
+            )}
           </div>
           
           <div className='book-details' >
