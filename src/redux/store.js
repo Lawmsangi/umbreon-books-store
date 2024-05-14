3989aa6e7be01a2ba479c092c5b7e8c6d091d2c6
Lawmsangi <ddarkphoenixx22@gmail.com>
2024-05-14 17:10:35 +0530

    Add address slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './cartsSlice';
+import addressReducer from './addressSlice';
 
 const store = configureStore({
     reducer: {
-        cart: cartReducer
+        cart: cartReducer,
+        address: addressReducer
     }
 });
 
